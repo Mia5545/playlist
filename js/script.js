76e05ef0ebb3117c8c1244fe6a3e294c5911342d
Mia Johnson <mia.johnson320@gmail.com>
2019-01-14 23:07:34 +0000

    playlist

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 /* global $ */
 
 // BELOW Update the songs array with four of your favorites songs.
-var songs = [];
+var songs = ["Talk to Me", "Initiation", "Champion", "Yosemite"];
 // BELOW Add 4 More arrays to store images_links, artists, song lengths, and links for each song
 // Make sure they match the same order as your array above
-
-
+var images = [];
+var artists = ["Tory Lanez", "TOry Lanez", "NAV", "Travis Scott"];
+var lengths = ["186", "276", "195", "152"];
+var links = ["https://www.youtube.com/results?search_query=talk+to+me", "https://www.youtube.com/results?search_query=initiation+tory+lanez", "https://www.youtube.com/results?search_query=champion+nav",
+"https://www.youtube.com/results?search_query=yosemite"];
 
 function displaySongInfo(){
     // BELOW Use forEach Loop to display the data from each of your arrays in the correct div
