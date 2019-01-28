bc4e28d52d3ef05e31c0c71e830872aa27f4b839
Mia Johnson <mia.johnson320@gmail.com>
2019-01-28 23:18:11 +0000

    PLaylist

--- js/script.js
+++ js/script.js
@@ -5,21 +5,23 @@ var songs = ["Talk to Me", "Initiation", "Champion", "Yosemite"];
 // BELOW Add 4 More arrays to store images_links, artists, song lengths, and links for each song
 // Make sure they match the same order as your array above
 var images = [];
-var artists = ["Tory Lanez", "TOry Lanez", "NAV", "Travis Scott"];
+var artists = ["Tory Lanez", "Tory Lanez", "NAV", "Travis Scott"];
 var lengths = ["186", "276", "195", "152"];
 var links = ["https://www.youtube.com/results?search_query=talk+to+me", "https://www.youtube.com/results?search_query=initiation+tory+lanez", "https://www.youtube.com/results?search_query=champion+nav",
 "https://www.youtube.com/results?search_query=yosemite"];
 
 function displaySongInfo(){
-    // BELOW Use forEach Loop to display the data from each of your arrays in the correct div
-
+    songs.forEach(function(song) {
+    $("#songs").append("<p> " + song + "</p>");
+    });
+    artists.forEach(function(artist) {
+    $("#artists").append("<p> " + artist + "</p>");
+});
 }
-
+    // BELOW Use forEach Loop to display the data from each of your arrays in the correct div
 function emptySongInfo(){
     $("#songs").empty();
     // Use jQuery to empty all of the remaining divs
-
-
 }
 
 
